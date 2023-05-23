3d9779d4838342b608ffb826b4e8a7a5db22cd29
Blizzard Finnegan <blizzardfinnegan@gmail.com>
2023-05-23 16:09:38 -0400

    Finish GPIO translation

--- src/gpio_facade.rs
+++ src/gpio_facade.rs
@@ -1,6 +1,9 @@
 use std::time::Duration;
-
-use rppal::gpio::{Gpio,OutputPin,InputPin};
+use async_std::sync::*;
+use rppal::gpio::{Gpio,OutputPin,InputPin,Trigger,Level};
+use std::thread;
+use std::result::Result;
+use std::fmt;
 
 const TRAVEL_DISTANCE_FACTOR:f64 = 0.95;
 const POLL_DELAY:Duration = Duration::from_millis(10);
@@ -12,40 +15,177 @@ const RUN_SWITCH_ADDR:u8 = 10;
 const UPPER_LIMIT_ADDR:u8 = 23;
 const LOWER_LIMIT_ADDR:u8 = 24;
 
+static MOVE_LOCK:RwLock<bool> = RwLock::new(true);
+
 pub struct Fixture{
     gpio_api:Gpio,
     travel_distance: u32,
-    safe_to_run: bool,
-    motor_direction:OutputPin,
-    motor_enable: OutputPin,
-    piston_enable: OutputPin,
-    upper_limit: InputPin,
-    lower_limit: InputPin,
-    run_switch: InputPin
+    motor_direction:Option<OutputPin>,
+    motor_enable: Option<OutputPin>,
+    piston_enable: Option<OutputPin>,
+    upper_limit: Option<InputPin>,
+    lower_limit: Option<InputPin>,
+}
+
+pub enum Direction{Up,Down}
+
+impl Drop for Fixture{
+    fn drop(&mut self) {
+        self.reset_arm();
+    }
+}
+
+#[derive(Debug,Clone)]
+pub struct GpioError;
+pub struct FixtureInitError;
+
+impl fmt::Display for GpioError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Bad GPIO settings for fixture")
+    }
+}
+impl fmt::Display for FixtureInitError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid PWM value for fixture")
+    }
 }
 
 impl Fixture{
-    pub fn new() -> Self{
-        let mut gpio = Gpio::new().unwrap();
-        let output = Self{
+    //modify implementation to allow for multiple fixtures simultaneously
+    pub fn new() -> Result<Self,GpioError>{
+        let gpio = Gpio::new().unwrap();
+        let mut output = Self{
             gpio_api:gpio,
-            safe_to_run:true,
             travel_distance: u32::MAX,
-            motor_direction: gpio.get(MOTOR_DIRECTION_ADDR).unwrap().into_output_low(),
-            motor_enable: gpio.get(MOTOR_ENABLE_ADDR).unwrap().into_output_low(),
-            piston_enable: gpio.get(PISTON_ADDR).unwrap().into_output_low(),
-            upper_limit: gpio.get(UPPER_LIMIT_ADDR).unwrap().into_input_pulldown(),
-            lower_limit: gpio.get(LOWER_LIMIT_ADDR).unwrap().into_input_pulldown(),
-            run_switch: gpio.get(RUN_SWITCH_ADDR).unwrap().into_input_pulldown(),
+            motor_direction: None,
+            motor_enable: None,
+            piston_enable: None,
+            upper_limit: None,
+            lower_limit: None,
         };
-        output.run_switch.set_async_interrupt(rppal::gpio::Trigger::RisingEdge, move||{
-            output.safe_to_run = false;
-            while output.run_switch.is_low() {}
-            output.safe_to_run = true;
-        });
+
+        output.motor_enable = Some(output.gpio_api.get(MOTOR_ENABLE_ADDR).unwrap().into_output_low());
+        output.motor_direction = Some(output.gpio_api.get(MOTOR_DIRECTION_ADDR).unwrap().into_output_low());
+        output.piston_enable = Some(output.gpio_api.get(PISTON_ADDR).unwrap().into_output_low());
+        output.upper_limit = Some(output.gpio_api.get(UPPER_LIMIT_ADDR).unwrap().into_input_pulldown());
+        output.lower_limit = Some(output.gpio_api.get(LOWER_LIMIT_ADDR).unwrap().into_input_pulldown());
+
+        let possible_run_pin = output.gpio_api.get(RUN_SWITCH_ADDR);
+        match possible_run_pin{
+            Ok(run_pin) =>{
+                _ = run_pin.into_input_pulldown().set_async_interrupt(Trigger::Both, |switch_state|{
+                    _ = async{
+                        let mut move_allowed = MOVE_LOCK.write().await;
+                        match switch_state {
+                            Level::Low=> {
+                                *move_allowed = false;
+                            }
+                            Level::High => {
+                                *move_allowed = true;
+                            }
+                            
+                        };
+                    };
+                });
+            },
+            Err(_) => {}
+        }
+
+        while let Err(_) = output.find_distance() {};
+
+        return Ok(output);
     }
 
-    fn reset_arm(&mut self) -> &mut Self{
-        //if(self.upper_limit.
+    fn reset_arm(&mut self) -> u16{
+        if let (Some(upper_limit),          Some(motor_direction_pin),      Some(motor_enable_pin)) = 
+               (self.upper_limit.as_mut(), self.motor_direction.as_mut(), self.motor_enable.as_mut()){
+            if upper_limit.is_high(){
+                {
+                    motor_direction_pin.set_low();
+                    motor_enable_pin.set_high()
+                }
+                thread::sleep(Duration::from_millis(500));
+                motor_enable_pin.set_low()
+            }
+            motor_direction_pin.set_high();
+            motor_enable_pin.set_high();
+            let mut counter = 0;
+            for _ in 0..TIMEOUT {
+                if upper_limit.is_high() { break; }
+                counter += 1;
+                thread::sleep(POLL_DELAY);
+            }
+            motor_enable_pin.set_low();
+            if counter < TIMEOUT { return counter; }
+        };
+        return 0;
+    }
+
+    fn find_distance(&mut self) -> Result<(),FixtureInitError>{
+        if self.reset_arm() == 0 { return Err(FixtureInitError); }
+        let mut down_counter:u32 = 0;
+        let mut up_counter:u32 = 0;
+
+        //Time travel time to lower limit switch 
+        if let (Some(lower_limit),            Some(motor_direction_pin),      Some(motor_enable_pin)) = 
+               (self.lower_limit.as_mut(), self.motor_direction.as_mut(), self.motor_enable.as_mut()){
+            motor_direction_pin.set_low();
+            motor_enable_pin.set_high();
+            for _ in 0..TIMEOUT{
+                if lower_limit.is_high() { break; }
+                down_counter += 1;
+                thread::sleep(POLL_DELAY);
+            }
+        }
+
+        //Time travel time to upper limit switch 
+        if let (Some(upper_limit),            Some(motor_direction_pin),      Some(motor_enable_pin)) = 
+               (self.upper_limit.as_mut(), self.motor_direction.as_mut(), self.motor_enable.as_mut()){
+            motor_direction_pin.set_low();
+            motor_enable_pin.set_high();
+            for _ in 0..TIMEOUT{
+                if upper_limit.is_high() { break; }
+                up_counter += 1;
+                thread::sleep(POLL_DELAY);
+            }
+        }
+
+        self.travel_distance = std::cmp::min(up_counter,down_counter);
+
+        return Ok(())
+    }
+
+    pub fn goto_limit(&mut self, direction:Direction) -> bool{
+        let ref mut limit_sense:InputPin;
+        match direction{
+            Direction::Down => {
+                if let Some(obj) = self.lower_limit.as_mut() {
+                    limit_sense = obj;
+                }
+                else { return false; }
+            },
+            Direction::Up => {
+                if let Some(obj) = self.upper_limit.as_mut() {
+                    limit_sense = obj;
+                }
+                else { return false; }
+            }
+        }
+
+        if limit_sense.is_high() { return true; }
+
+        let move_polls = (self.travel_distance as f64 * TRAVEL_DISTANCE_FACTOR) as u64;
+
+        if let (Some(motor_direction_pin),      Some(motor_enable_pin)) = 
+               (self.motor_direction.as_mut(), self.motor_enable.as_mut()){
+            motor_direction_pin.set_low();
+            motor_enable_pin.set_high();
+            for _ in 0..move_polls{
+                if limit_sense.is_high() { break; }
+                thread::sleep(POLL_DELAY);
+            }
+        }
+
+        return limit_sense.is_high();
     }
 }
